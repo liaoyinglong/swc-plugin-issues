b12c5d4c5fc7b3382649cad3478f98fe18443665
liaoyl <liaoyl@duneproject.xyz>
2022-09-26 18:19:23 +0800

    init

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  experimental: {
+    swcPlugins: [
+      //
+      ["@swc/plugin-emotion", {}]
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
